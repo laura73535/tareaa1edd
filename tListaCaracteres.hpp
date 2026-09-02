40513991f39fc9567acfac6fa3182b04dc02f514
Fiorella <fiorellavillaa@gmail.com>
2026-09-01 22:31:13 -0400

    aura laura

--- tListaCaracteres.hpp
+++ tListaCaracteres.hpp
@@ -1,15 +1,19 @@
+#ifndef TLISTACARACTERES_HPP
+#define TLISTACARACTERES_HPP
 
-struct tListaCaracteres
-{
-    char* listArray;
-    int listSize;
-    int maxSize;
-    int curr;
+// Estructura del arreglo dinamico para los caracteres
+struct tListaCaracteres {
+    char* listArray; // Puntero al arreglo en memoria dinamica
+    int listSize;    // Longitud actual de la linea
+    int maxSize;     // Capacidad maxima del arreglo
+    int curr;        // Indice del cursor interno
 };
 
-void inicilizarListas(){}
-void insertarCaractere(){}
-void borrarCaracter(){}
-void concatenarTexto(){}
-void liberarLista(){}
+// Firmas de las funciones del TDA
+void inicializarLista(tListaCaracteres &lista, int capacidadInicial);
+void insertarCaracter(tListaCaracteres &lista, int pos, char c);
+char borrarCaracter(tListaCaracteres &lista, int pos);
+void concatenarTexto(tListaCaracteres &lista, const char* texto, int len);
+void liberarLista(tListaCaracteres &lista);
 
+#endif
