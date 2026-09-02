ffc6f96aa23a5eb9371b84bb0ef6733d7a709a3e
laura73535 <lorellanaa@usm.cl>
2026-09-02 11:26:32 -0400

    hola

--- tParrafo.hpp
+++ tParrafo.hpp
@@ -7,18 +7,18 @@
 struct tNodoLinea {
     tNodoLinea* sig;           // Unicamente enlace hacia adelante
     int nro_linea_parrafo;     // Indice de la linea
-    tListaCaracteres info;     // Los datos del Nivel 1[cite: 1]
+    tListaCaracteres info;     // Los datos del Nivel 1
 };
 
 // Estructura del TDA Segundo Nivel: tParrafo
 struct tParrafo {
-    tNodoLinea* headLinea;     // Puntero al inicio de la lista[cite: 1]
-    tNodoLinea* tailLinea;     // Puntero al final de la lista[cite: 1]
-    int cantLineas;            // Cantidad total de lineas[cite: 1]
+    tNodoLinea* headLinea;     // Puntero al inicio de la lista
+    tNodoLinea* tailLinea;     // Puntero al final de la lista
+    int cantLineas;            // Cantidad total de lineas
     int id_parrafo;            // Identificador unico del parrafo
 };
 
-// Firmas de las funciones del TDA[cite: 1]
+// Firmas de las funciones del TDA
 void inicializarParrafo(tParrafo &parrafo, int idParrafo);
 void agregarLineaAlFinal(tParrafo &parrafo, const tListaCaracteres &linea);
 void fusionarLineas(tParrafo &parrafo, int nroL1, int nroL2);
